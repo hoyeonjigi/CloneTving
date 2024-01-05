19b025736c7881a0f86ce9e75f07407432b44777
minsik2434 <minsik2434@gmail.com>
2024-01-05 12:51:49 +0900

    Feat : 프로필 이미지관련 코드 작성
    프로필 이미지 저장 및 변환 등 메소드 추가

--- clone-tving/src/main/java/site/hoyeonjigi/clonetving/repository/ProfileImageRepository.java
+++ clone-tving/src/main/java/site/hoyeonjigi/clonetving/repository/ProfileImageRepository.java
@@ -1,6 +1,10 @@
 package site.hoyeonjigi.clonetving.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import site.hoyeonjigi.clonetving.domain.ProfileImageEntity;
@@ -8,4 +12,6 @@ import site.hoyeonjigi.clonetving.domain.ProfileImageEntity;
 @Repository
 public interface ProfileImageRepository extends JpaRepository<ProfileImageEntity, String> {
     
+    @Query(value = "select pi from ProfileImageEntity pi where pi.imageId = :imageId")
+    List<ProfileImageEntity> findProfileImage(@Param("imageId")String imageId);
 }
--- clone-tving/src/test/java/site/hoyeonjigi/clonetving/repository/ProfileImageRepositoryTest.java
+++ clone-tving/src/test/java/site/hoyeonjigi/clonetving/repository/ProfileImageRepositoryTest.java
@@ -1,9 +1,22 @@
 package site.hoyeonjigi.clonetving.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import site.hoyeonjigi.clonetving.domain.GenreEntity;
+import site.hoyeonjigi.clonetving.domain.ProfileImageEntity;
 
 @SpringBootTest
 @Transactional
@@ -12,8 +25,44 @@ public class ProfileImageRepositoryTest {
     @Autowired
     ProfileImageRepository profileImageRepository;
 
+    void save(String imageId, String imageName, byte[] imageBolb){
+        ProfileImageEntity profileImageEntity = ProfileImageEntity.builder()
+                                                    .imageId(imageId)
+                                                    .imageName(imageName)
+                                                    .imageBlob(imageBolb).build();
+        ProfileImageEntity saveProfileImageEntity = profileImageRepository.save(profileImageEntity);
+        assertEquals(profileImageEntity, saveProfileImageEntity);
+    }
+
+
     @Test
-    void saveProfileImage(){
-        
+    void saveProfileImage() throws IOException{
+        byte[] imagetobyte = imageTobyteArray("/Users/choiminsik/Downloads/default1.png");
+        save("1001", "기본1", imagetobyte);
+    }
+
+    @Test
+    void loadImage(){
+        ProfileImageEntity findImageEntity = profileImageRepository.findProfileImage("1001").get(0);
+        byte[] loadimage = findImageEntity.getImageBlob();
+        byteArrayToImage(loadimage);
+    }
+
+    public static byte[] imageTobyteArray(String imagePath) throws IOException{
+        byte[] imageInByte;
+        imageInByte = Files.readAllBytes(Paths.get(imagePath));
+        return imageInByte;
+    }
+
+    public void byteArrayToImage(byte[] imageData){
+        String formatName = "PNG";
+        try {
+            BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(imageData));
+            File outputImage = new File("/Users/choiminsik/Downloads/outputImage.png"); // 저장할 파일 경로 및 이름
+            ImageIO.write(bufferedImage, formatName, outputImage);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
