046a162812c41db50be8d8bf8ec3f611f0eef018
Yu Ho Yeon <zmfmsh46@gmail.com>
2024-01-19 14:57:13 +0900

    Fix : 스웨거 접속 오류 해결
    /swagger-ui/index.html 접속 시, 권한이 없어 403에러가 뜨는 것을
    발견하였고, WebSecurityConfig의 필터부분에서 해당url에 대한 권한 설정이
    없어 요청이 거부됬음을 확인 후 권한을 허용해주는 코드 추가.

--- clone-tving/src/main/java/site/hoyeonjigi/clonetving/repository/ProfileRepository.java
+++ clone-tving/src/main/java/site/hoyeonjigi/clonetving/repository/ProfileRepository.java
@@ -1,18 +1,11 @@
 package site.hoyeonjigi.clonetving.repository;
 
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import site.hoyeonjigi.clonetving.domain.ProfileEntity;
 import site.hoyeonjigi.clonetving.domain.ProfileEntityPK;
 import site.hoyeonjigi.clonetving.domain.UserEntity;
 
 public interface ProfileRepository extends JpaRepository<ProfileEntity, ProfileEntityPK>{
     Optional<ProfileEntity> findByUserAndProfileName(UserEntity user, String ProfileName);
-    List<ProfileEntity> findByUser(UserEntity user);
-    void deleteByUserAndProfileName(UserEntity user , String profileName);
-    void delete(ProfileEntity profileEntity);
-    void deleteById(ProfileEntityPK prifilEntityPK);
 }
