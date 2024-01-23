5406e9fe0d55002a6972e84a5c6e9b5a2497277e
ewsn0825 <ewsn0825@naver.com>
2024-01-23 18:01:41 +0900

    유저 프로필 이미지 모달창 구현

--- TvingFront/src/components/modal/UserProfileModal.jsx
+++ TvingFront/src/components/modal/UserProfileModal.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
+import { motion } from "framer-motion";
+
 import "swiper/css";
 import "swiper/css/navigation";
 
@@ -29,7 +29,10 @@ import yumi6 from "@/assets/profiles/yumi/yumi_6.png";
 import yumi7 from "@/assets/profiles/yumi/yumi_7.png";
 import yumi8 from "@/assets/profiles/yumi/yumi_8.png";
 import yumi9 from "@/assets/profiles/yumi/yumi_9.png";
-import SwiperEX from "./SwiperEX";
+
+import close from "@/assets/profiles/icon_x.svg";
+import right from "@/assets/profiles/icon-right.svg";
+import left from "@/assets/profiles/icon-left.svg";
 
 function UserProfileModal({ isOpen, closeModal }) {
   if (!isOpen) {
@@ -61,57 +64,140 @@ function UserProfileModal({ isOpen, closeModal }) {
     { src: yumi9, alt: "유미의 세포들 프로필 9" },
   ];
 
+  const [baseSlideIndex, setBaseSlideIndex] = useState(0);
+  const [yumiSlideIndex, setYumiSlideIndex] = useState(0);
+
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 ">
-      <div className="bg-gray_01 rounded-lg flex flex-col w-[60%] h-[90%] items-center">
-        <span className="float-right cursor-pointer" onClick={closeModal}>
-          &times;
-        </span>
+      <div className="bg-gray_01 rounded-lg flex flex-col w-[60%] h-[93%] items-center relative">
+        <button onClick={closeModal} className="absolute right-1">
+          <img src={close} alt="" className="w-16" />
+        </button>
 
-        {/* <SwiperEX /> */}
-        <h3 className="text-white text-3xl">이미지 선택</h3>
-        <div>
-          <h4 className="text-white">기본 이미지</h4>
-          <Swiper
-            spaceBetween={10}
-            slidesPerView={5}
-            // navigation
-            navigation={{
-              nextEl: ".swiper-button-next-base",
-              prevEl: ".swiper-button-prev-base",
-            }}
-            modules={[Navigation]}
-            className="w-[800px]"
-          >
-            {baseProfiles.map((profile, index) => (
-              <SwiperSlide key={index}>
-                <img src={profile.src} alt={profile.alt} />
-              </SwiperSlide>
-            ))}
-            <div className="swiper-button-next-base"></div>
-            <div className="swiper-button-prev-base"></div>
-          </Swiper>
+        <h3 className="text-white text-3xl mt-16 mb-10">이미지 선택</h3>
 
-          <h4 className="text-white">유미의 세포들 이미지</h4>
-          <Swiper
-            spaceBetween={10}
-            slidesPerView={5}
-            // navigation
-            navigation={{
-              nextEl: ".swiper-button-next-yumi",
-              prevEl: ".swiper-button-prev-yumi",
-            }}
-            modules={[Navigation]}
-            className="w-[800px]"
-          >
-            {yumiProfiles.map((profile, index) => (
-              <SwiperSlide key={index}>
-                <img src={profile.src} alt={profile.alt} />
-              </SwiperSlide>
-            ))}
-            <div className="swiper-button-next-yumi"></div>
-            <div className="swiper-button-prev-yumi"></div>
-          </Swiper>
+        <div className="mx-16 w-full">
+          <div className="relative px-[4.5rem]">
+            <h4 className="text-white text-1.5xl">기본 이미지</h4>
+            <Swiper
+              spaceBetween={10}
+              slidesPerView={5}
+              navigation={{
+                nextEl: ".swiper-button-next-base",
+                prevEl: ".swiper-button-prev-base",
+              }}
+              modules={[Navigation]}
+              slidesPerGroup={1}
+              onSlideChange={(swiper) => {
+                if (swiper.isEnd) {
+                  setBaseSlideIndex(baseProfiles.length - 1);
+                } else {
+                  setBaseSlideIndex(swiper.realIndex);
+                }
+              }}
+            >
+              {baseProfiles.map((profile, index) => (
+                <SwiperSlide key={index}>
+                  <motion.img
+                    src={profile.src}
+                    alt={profile.alt}
+                    whileHover={{ y: -15 }} // 마우스 호버 시 y축으로 -10 이동
+                    transition={{
+                      type: "tween",
+                      stiffness: 300,
+                      duration: 0.2,
+                    }}
+                    className="mt-4 rounded-sm"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <button
+              className={`swiper-button-prev-base ${
+                baseSlideIndex === 0 ? "opacity-30" : "opacity-100"
+              }`}
+            >
+              <img
+                src={left}
+                alt=""
+                className=" absolute top-[55%] left-3 transform -translate-y-1/2 w-[7%]"
+              />
+            </button>
+            <button
+              className={`swiper-button-next-base ${
+                baseSlideIndex === baseProfiles.length - 1
+                  ? "opacity-30"
+                  : "opacity-100"
+              }`}
+            >
+              <img
+                src={right}
+                alt=""
+                className=" absolute top-[55%] right-3 transform -translate-y-1/2 w-[7%]"
+              />
+            </button>
+          </div>
+
+          <div className="relative mt-10 px-[4.5rem]">
+            <h4 className="text-white text-1.5xl">유미의 세포들 이미지</h4>
+            <Swiper
+              spaceBetween={10}
+              slidesPerView={5}
+              navigation={{
+                nextEl: ".swiper-button-next-yumi",
+                prevEl: ".swiper-button-prev-yumi",
+              }}
+              modules={[Navigation]}
+              slidesPerGroup={1}
+              onSlideChange={(swiper) => {
+                if (swiper.isEnd) {
+                  setYumiSlideIndex(yumiProfiles.length - 1);
+                } else {
+                  setYumiSlideIndex(swiper.realIndex);
+                }
+              }}
+            >
+              {yumiProfiles.map((profile, index) => (
+                <SwiperSlide key={index}>
+                  <motion.img
+                    src={profile.src}
+                    alt={profile.alt}
+                    whileHover={{ y: -15 }} // 마우스 호버 시 y축으로 -10 이동
+                    transition={{
+                      type: "tween",
+                      stiffness: 300,
+                      duration: 0.2,
+                    }}
+                    className="mt-4 rounded-sm"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <button
+              className={`swiper-button-prev-yumi ${
+                yumiSlideIndex === 0 ? "opacity-30" : "opacity-100"
+              }`}
+            >
+              <img
+                src={left}
+                alt=""
+                className=" absolute top-[55%] left-3 transform -translate-y-1/2 w-[7%]"
+              />
+            </button>
+            <button
+              className={`swiper-button-next-yumi ${
+                yumiSlideIndex === yumiProfiles.length - 1
+                  ? "opacity-30"
+                  : "opacity-100"
+              }`}
+            >
+              <img
+                src={right}
+                alt=""
+                className=" absolute top-[55%] right-3 transform -translate-y-1/2 w-[7%]"
+              />
+            </button>
+          </div>
         </div>
       </div>
     </div>
