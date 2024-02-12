ce9cbbdb1e5904cbfad7e5e027419641bee6deb9
minsik2434 <minsik2434@gmail.com>
2024-02-12 16:15:18 +0900

    Fix : 콘텐츠 API 명세 수정

--- clone-tving/src/main/java/site/hoyeonjigi/clonetving/controller/ContentController.java
+++ clone-tving/src/main/java/site/hoyeonjigi/clonetving/controller/ContentController.java
@@ -17,28 +17,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/contents")
 public class ContentController {
     
     private final ContentService contentService;
 
-    @RequestMapping(value="/api/release-date/classification={classification}", method=RequestMethod.GET)
+    @RequestMapping(value="/{classification}/lastest20", method=RequestMethod.GET)
 	public List<ContentDto> openReleasedContentList(@PathVariable("classification") String classification) throws Exception{
 		return contentService.selectReleasedContent(classification);
 	}
 
-	@RequestMapping(value="/api/contentbygenre/classification={classification}/genre={genre}", method=RequestMethod.GET)
+	@RequestMapping(value="/{classification}/{genre}", method=RequestMethod.GET)
 	public List<ContentDto> openContentByGenre(@PathVariable("classification")String classification,
 	 	@PathVariable("genre")String genre, @RequestParam(value="page",defaultValue = "0")int pageNumber) throws Exception {
 		return contentService.selectContentByGenre(classification, genre, pageNumber);
 	}
 	
-	@RequestMapping(value="/api/popular/classification={classification}", method=RequestMethod.GET)
+	@RequestMapping(value="/{cla ssification}/popular", method=RequestMethod.GET)
 	public List<ContentDto> openPopularContent(@PathVariable("classification")String classification,
 												@RequestParam(value="page",defaultValue = "0")int pageNumber) throws Exception{
 		return contentService.selectPopularContent(classification,pageNumber);
 	}	
 
-	@RequestMapping(value="/api/content/title={contenttitle}", method=RequestMethod.GET)
+	@RequestMapping(value="/{contenttitle}", method=RequestMethod.GET)
 	public List<ContentDto> openContentByTitle(@PathVariable("contenttitle")String contentTitle,
 												@RequestParam(value="page",defaultValue = "0")int pageNumber) throws UnsupportedEncodingException{
 		return contentService.selectContentByTitle(contentTitle,pageNumber);
