b6c3bb495d31b8dbe98db3a30c3dbb22b124ae02
ewsn0825 <ewsn0825@naver.com>
2024-04-08 17:27:24 +0900

    Refactor: 메인 유저 모달 코드 리팩토링

--- TvingFront/src/store/useContent.js
+++ TvingFront/src/store/useContent.js
@@ -1,21 +1,46 @@
-import { create } from "zustand";
+// import { create } from "zustand";
 
-const contentStore = (set) => ({
-  // contentId: "",
-  // contentTitle: "",
-  // contentOverview: "",
-  // genreIds: [],
+// const contentStore = (set) => ({
+//   // contentId: "",
+//   // contentTitle: "",
+//   // contentOverview: "",
+//   // genreIds: [],
+//   content: {},
+//   genre: [],
+
+//   // setContentId: (contentId) => set({ contentId }),
+//   // setContentTitle: (contentTitle) => set({ contentTitle }),
+//   // setContentOverview: (contentOverview) => set({ contentOverview }),
+//   // setGenreIds: (genreIds) => set({ genreIds }),
+//   setContent: (content) => set({ content }),
+//   setGenre: (genre) => set({ genre }),
+// });
+
+// const useContent = create(contentStore);
+
+// export default useContent;
+
+import { create } from "zustand";
+// zustand/middleware에서 persist를 임포트합니다.
+import { persist } from "zustand/middleware";
+
+const contentsStore = (set) => ({
   content: {},
   genre: [],
-
-  // setContentId: (contentId) => set({ contentId }),
-  // setContentTitle: (contentTitle) => set({ contentTitle }),
-  // setContentOverview: (contentOverview) => set({ contentOverview }),
-  // setGenreIds: (genreIds) => set({ genreIds }),
   setContent: (content) => set({ content }),
   setGenre: (genre) => set({ genre }),
 });
 
-const useContent = create(contentStore);
+// create 함수를 호출할 때 persist 함수로 contentStore를 감싸줍니다.
+// persist 함수는 첫 번째 파라미터로 스토어 설정을 받고, 두 번째 파라미터로 옵션 객체를 받습니다.
+const useContents = create(
+  persist(contentsStore, {
+    name: "contents", // 로컬 스토리지에 저장될 때 사용될 키 이름입니다.
+    // 여기서는 'content-storage'라는 이름으로 저장됩니다.
+    // getStorage 함수를 제공하여 사용할 스토리지 타입을 지정할 수 있습니다.
+    // 기본값은 localStorage입니다. sessionStorage를 사용하려면 아래와 같이 설정하세요.
+    // getStorage: () => sessionStorage,
+  })
+);
 
-export default useContent;
+export default useContents;
